0f809c207f90d8d28bcdafa1af2bc8c9dd9306b1
mfwolffe <wolffemf@dukes.jmu.edu>
2026-04-27 15:14:31 -0400

    Fix Mocha UI: use TDD to match suite/test test syntax

--- test/suite/index.ts
+++ test/suite/index.ts
@@ -3,7 +3,7 @@ import Mocha from "mocha";
 import { glob } from "glob";
 
 export async function run(): Promise<void> {
-  const mocha = new Mocha({ ui: "bdd", timeout: 30_000 });
+  const mocha = new Mocha({ ui: "tdd", timeout: 30_000 });
 
   const testsRoot = path.resolve(__dirname);
   const files = await glob("**/*.test.js", { cwd: testsRoot });
